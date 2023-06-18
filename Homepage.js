67eed6d132a5c2b3efd047258f0280d3f8f0b2dc
Eskoo59 <iliesbelmir@outlook.fr>
2023-06-18 18:48:54 +0200

    newCommit

--- Homepage.js
+++ Homepage.js
@@ -12,46 +12,73 @@ document.addEventListener("wheel", (evt) => {
 });
 
 /*-------------------------Theme Change------------------------------ */
-const btn = document.querySelector('#btn');
-btn.addEventListener("click", changeTheme2);
-let light = true;
+
+  const btn = document.querySelector("#btn");
+  btn.addEventListener("click", changeTheme2);
+  
+  let light = true;
+  loadThemeFromLocalStorage(); 
+  
+  function changeTheme2() {
+    if (light) {
+      setDarkTheme();
+    } else {
+      setLightTheme();
+    }
+    saveThemeToLocalStorage(); 
+  }
+  
+  function setDarkTheme() {
+    document.documentElement.style.setProperty("--main-bg-color", "#000000");
+    document.documentElement.style.setProperty("--text-color", "#FFFFFF");
+    document.querySelector("#background-video source").src = "image/vecteezy_colourful-particle-moving-on-balck-background-blue-smoke_7515486_420.mp4";
+    const video = document.querySelector("#background-video");
+  video.load();
+  video.play();
+    light = false;
+  }
+  
+  function setLightTheme() {
+    document.documentElement.style.setProperty("--main-bg-color", "#FFFFFF");
+    document.documentElement.style.setProperty("--text-color", "#000000");
+    document.querySelector("#background-video source").src = "image/white_background_-_92723 (Original).mp4";
+    const video = document.querySelector("#background-video");
+  video.load();
+  video.play();
+    light = true;
+  }
+  
+  /* ------------ localStorage --------------- */
+  function loadThemeFromLocalStorage() {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      setDarkTheme();
+    } else {
+      setLightTheme();
+    }
+  }
+  
+  function saveThemeToLocalStorage() {
+    if (light) {
+      localStorage.removeItem("theme");
+    } else {
+      localStorage.setItem("theme", "dark");
+    }
+  }
+  
+/*-----------------------------------------*/
+
+
 function changeTheme2() {
     if (light) {
-        document.documentElement.style.setProperty("--main-bg-color", "#000000");
-        document.documentElement.style.setProperty("--text-color", "#FFFFFF");
-        localStorage.setItem("theme", "dark");
-        light = false;
+        setDarkTheme();
+        btn.style.color = "white"; 
+    } else {
+        setLightTheme();
+        btn.style.color = "black";
     }
-    else {
-        document.documentElement.style.setProperty("--main-bg-color", "#FFFFFF");
-        document.documentElement.style.setProperty("--text-color", "#000000");
-        localStorage.removeItem("theme");
-        light = true;
-    }
-}
-
-/* ---------------------- Intersection observer ----------------*/
-
-
-const proj = document.querySelector("section div")
-
-const observe = new IntersectionObserver(setAnimation)
-
-function setAnimation(entries) {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            proj.style.opacity = 1;
-            proj.style.animationName = "appear";
-            proj.style.animationDuration = "1s";
-            proj.style.animationTimingFunction = "ease-in";
-            proj.style.animationFillMode = "forwards";
-        } else {
-            proj.style.opacity = 0;
-        }
-    });
+    saveThemeToLocalStorage();
 }
 
 
-
-observe.observe(proj)
 
